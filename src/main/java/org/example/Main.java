e8ba343c51cdef180b767be47ff018e0d4c16546
Valentina <vale_colmenares@hotmail.com>
2025-03-13 22:31:51 -0500

    Se agregó funcionalidad

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,22 +5,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        //System.out.println("Hello, JavaBank ATM!");
+        System.out.println("Welcome to JavaBank ATM! Version control with Git.");
 
-        try{
-            int result = 10/0;
-        } catch(ArithmeticException e){
-            System.out.println("No se puede dividor por cero!");
-        }
-
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        /*System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }*/
     }
 }
